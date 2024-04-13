9ea041a8d09bdc4e25e34ad89380c5330ad25c57
Ubuntu <ubuntu@ip-172-31-1-254.ap-southeast-2.compute.internal>
2024-04-13 23:36:16 +0000

    ec2

---
+++ confs/gunicorn_conf.py
@@ -0,0 +1,4 @@
+command = '/home/ubuntu/Style_Hub/venv/bin/gunicorn'
+pythonpath ='/home/ubuntu/Style_Hub'
+bind ='172.31.1.254:8000'
+workers =1
--- style_hub/settings.py
+++ style_hub/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['102.135.173.110','3.27.181.62']
 CORS_ORIGIN_ALLOW_ALL = True
 
 
@@ -140,11 +140,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 
-STATIC_URL = 'static/'
-# STATIC_ROOT = os.path.join(BASE_DIR,'static')
-MEDIA_URLS = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'Media')
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
